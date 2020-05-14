92ad59f2fb958a51595114b2ef0220f176ce1f87
John Aponte <john.j.aponte@gmail.com>
2020-05-14 11:26:48 +0200

    Modifiy the templates. Now one for the heading, and one for the closure

--- R/template.R
+++ R/template.R
@@ -2,14 +2,28 @@
 # 20200512 by JJAV
 # # # # # # # # # # # # # # # # # # # #
 
-#' RStudio addin app to insert a template to initiate a snip code
+#' RStudio addin app to insert a template to the heading part of a snip code
 #'
 #' @return The template to insert
 #' @export
 #' @importFrom rstudioapi insertText
-insert_template <- function(){
+insert_apen_template <- function(){
   # This function is not included in the unit test
   fpath <- system.file("templates", "template_snip.txt", package="repana")
+  txt <- readLines(fpath)
+  txt <- paste(txt, collapse  = "\n")
+  txt <- gsub("XXXXX", format(Sys.Date()),txt)
+  rstudioapi::insertText(txt)
+}
+
+#' RStudio addin app to insert a template at the close part of a snip code
+#'
+#' @return The template to insert
+#' @export
+#' @importFrom rstudioapi insertText
+insert_closetemplate <- function(){
+  # This function is not included in the unit test
+  fpath <- system.file("templates", "template_close.txt", package="repana")
   txt <- readLines(fpath)
   txt <- paste(txt, collapse  = "\n")
   txt <- gsub("XXXXX", format(Sys.Date()),txt)
