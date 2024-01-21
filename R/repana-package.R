02461d1599bff93470b30820ce137383d6aa2c4f
johnaponte <john.j.aponte@gmail.com>
2024-01-21 19:22:02 +0000

    Update package documenntation and Structure vignette

--- R/repana-package.R
+++ R/repana-package.R
@@ -1,15 +1,5 @@
-#' repana
-#'
-#' Reproducible Analysis in R.
-#'
-#' Set of utilities to make easy the reproduction of analysis in R.
-#' It allow to make_structure, clean_structure, and run and log programs in a
-#' predefined order to allow secondary files, analysis and reports be constructed in
-#' and ordered form.
-#'
-#' @docType package
-#' @name repana
-NULL
+#' @keywords internal
+"_PACKAGE"
 
 utils::globalVariables(
   c(
